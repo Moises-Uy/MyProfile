cd65b09243f43f17323d7536795ecf90ea328ae5
Moises Uy <it.moisesuy@gmail.com>
2024-03-06 20:29:27 +0800

    Pushing to github

---
+++ app/components/constants.tsx
@@ -0,0 +1,20 @@
+type NAV_BAR = {
+  title: string;
+  path: string;
+};
+
+const constants: NAV_BAR[] = [
+  {
+    title: "About",
+    path: "/",
+  },
+  {
+    title: "Skill",
+    path: "/",
+  },
+  {
+    title: "Socials",
+    path: "/",
+  },
+];
+export default constants;
